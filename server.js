994db34f965522750a056ece0db05dff9917fe80
origin <mishalayubkhan10@gmail.com>
2025-03-15 00:25:09 +0530

    Initial backend commit

---
+++ server.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Database Connection
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error(err));
+
+// Routes
+const taskRoutes = require("./routes/taskroutes");
+app.use("/api/tasks", taskRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
